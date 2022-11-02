6d2f8dc3c1f394b27efbb7058cccc2182ddb19b4
Dan Garro <dobetterwithdan@gmail.com>
2022-11-02 15:06:19 -0400

    created folders screens and images, made directoryscreen and maincomponents

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/MainComponent'; 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Android Virtualization!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />; 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#61dafb',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
