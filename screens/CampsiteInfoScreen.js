45e5b9162661f907b4f0355734935b3e823faecc
Dan Garro <dobetterwithdan@gmail.com>
2022-11-11 15:45:24 -0500

    task 1 - workshop - buggy

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,13 +1,15 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Button, Modal, FlatList, StyleSheet, Text, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { useState } from 'react'; 
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false); 
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -40,9 +42,26 @@ const CampsiteInfoScreen = ({ route }) => {
                         markFavorite={() => dispatch(toggleFavorite(campsite.id))}
                     />
                     <Text style={styles.commentsTitle}>Comments</Text>
+                    <Modal
+                        animationType='slide'
+                        transparent={false}
+                        visible={showModal}
+                        onRequestClose={() => setShowModal(!showModal)}
+                    >
+                        <View style={styles.modal}>
+                            <View style={{margin: 10}}>
+                                <Button 
+                                    onPress={() => {setShowModal(!showModal);}}
+                                    color='#808080'
+                                    title='Cancel' 
+                                ></Button>
+                            </View>
+                        </View>
+                    </Modal>
                 </>
             }
         />
+        
     );
 };
 
@@ -60,6 +79,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
     }
 });
 
