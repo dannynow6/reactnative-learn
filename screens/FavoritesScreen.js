b983cec312c3e3b91473797ff5b8560e3fdf40be
Dan Garro <dobetterwithdan@gmail.com>
2022-11-16 14:57:56 -0500

    alert

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,27 +1,57 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    View,
+    FlatList,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Alert
+} from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../components/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-import { useDispatch } from 'react-redux'; 
 import { SwipeRow } from 'react-native-swipe-list-view';
-import { toggleFavorite } from '../features/favorites/favoritesSlice'; 
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector(
         (state) => state.campsites
     );
     const favorites = useSelector((state) => state.favorites);
-
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
 
     const renderFavoriteItem = ({ item: campsite }) => {
         return (
             <SwipeRow rightOpenValue={-100}>
                 <View style={styles.deleteView}>
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={styles.deleteTouchable}
-                        onPress={() => dispatch(toggleFavorite(campsite.id))}
+                        onPress={() =>
+                            Alert.alert(
+                                'Delete Favorite?',
+                                'Are you sure you wish to delete the favorite campsite ' +
+                                    campsite.name +
+                                    '?',
+                                [
+                                    {
+                                        text: 'Cancel',
+                                        onPress: () =>
+                                            console.log(
+                                                campsite.name + 'Not Deleted'
+                                            ),
+                                        style: 'cancel'
+                                    },
+                                    {
+                                        text: 'OK',
+                                        onPress: () =>
+                                            dispatch(
+                                                toggleFavorite(campsite.id)
+                                            )
+                                    }
+                                ],
+                                { cancelable: false }
+                            )
+                        }
                     >
                         <Text style={styles.deleteText}>Delete</Text>
                     </TouchableOpacity>
@@ -35,7 +65,10 @@ const FavoritesScreen = ({ navigation }) => {
                             })
                         }
                     >
-                        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + campsite.image }}
+                        />
                         <ListItem.Content>
                             <ListItem.Title>{campsite.name}</ListItem.Title>
                             <ListItem.Subtitle>
@@ -77,12 +110,12 @@ const styles = StyleSheet.create({
         flex: 1
     },
     deleteTouchable: {
-        backgroundColor: 'red', 
-        height: '100%', 
+        backgroundColor: 'red',
+        height: '100%',
         justifyContent: 'center'
     },
     deleteText: {
-        color: 'white', 
+        color: 'white',
         fontWeight: '700',
         textAlign: 'center',
         fontSize: 16,
@@ -91,3 +124,4 @@ const styles = StyleSheet.create({
 });
 
 export default FavoritesScreen;
+
