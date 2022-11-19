72a666f974656f99e6647973566d9d0b740ddac5
Dan Garro <dobetterwithdan@gmail.com>
2022-11-19 10:58:45 -0500

    gesture added to render

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -6,7 +6,8 @@ import {
     StyleSheet,
     Switch,
     Button,
-    Alert
+    Alert,
+    PanResponder
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
